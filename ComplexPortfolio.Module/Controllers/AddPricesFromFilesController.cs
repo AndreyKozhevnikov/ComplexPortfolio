dc5dc304241fc794b35998678c5b54f4c76d56a9
Andrey <andrey.kozhevnikov@devexpress.com>
2021-09-04 19:53:07 +0300

    add classes

---
+++ ComplexPortfolio.Module/Controllers/AddPricesFromFilesController.cs
@@ -0,0 +1,35 @@
+﻿using ComplexPortfolio.Module.BusinessObjects;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexPortfolio.Module.Controllers {
+   public class AddPricesFromFilesController:ObjectViewController<ListView,TickerPrice> {
+        public AddPricesFromFilesController() {
+            var addPricesAction = new SimpleAction(this, "AddPricesAction", PredefinedCategory.Edit);
+            addPricesAction.Execute += AddPricesAction_Execute; ;
+        }
+
+        private void AddPricesAction_Execute(object sender, SimpleActionExecuteEventArgs e) {
+            var folder = @"c:\temp\shares";
+            string[] fileEntries = Directory.GetFiles(folder);
+            foreach (var fileName in fileEntries) {
+                StreamReader file =    new StreamReader(fileName);
+                string line;
+                while((line= file.ReadLine()) != null) {
+
+                }
+
+            }
+        }
+        TickerPrice GetPriceFromLine(string line) {
+            return null;
+        }
+    }
+}
